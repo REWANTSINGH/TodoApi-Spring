2157414052aa417b89e07a0cebbb272d87398c5f
REWANTSINGH <bhatirewant2611@gmail.com>
2025-07-19 17:28:17 +0530

    Handled status code in response by responseEntity

--- src/main/java/com/example/todoapi/TodoController.java
+++ src/main/java/com/example/todoapi/TodoController.java
@@ -1,9 +1,8 @@
 package com.example.todoapi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,14 +21,22 @@ public class TodoController {
     }
 
     @GetMapping("/todos")
-    public List<Todo> getTodos(){
-        return this.todos;
+    public ResponseEntity<List<Todo>> getTodos(){
+//        return ResponseEntity.status(HttpStatus.CREATED).body(this.todos);
+        return ResponseEntity.ok(this.todos);
+
+
     }
 
     @PostMapping("/todos")
-    public Todo createTodo(@RequestBody Todo ntodo){
+//    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo ntodo){
+
+//        we can use this annotation @ResponseStatus(HttpStatus.CREATED) for response status or create class RESPONSE ENTITY
+
         todos.add(ntodo);
-        return ntodo;
+        return ResponseEntity.status(HttpStatus.CREATED).body(ntodo);
+//        return ResponseEntity.created(ntodo); but here this is not valid
     }
 
 
